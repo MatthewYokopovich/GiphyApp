adc5758d6c2358cb71cba761b656be12000d6a01
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-07-24 20:06:30 -0500

    More changes to logic.js and formatting

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -33,16 +33,21 @@ function display(tagstr) {
                 ids.push(element.id);
                 var newdiv = $("<div>");
                 var newstr = $("<p>");
-                newstr.text(element.rating);
+                newstr.text("Rating: "+element.rating);
                 newdiv.append(newstr);
-                var newimg = $("<img>").attr("src", element.images.fixed_height.url);
+                var newimg = $("<img>").attr("src", element.images.fixed_height_still.url);
+                newimg.attr("data-still", element.images.fixed_height_still.url);
+                newimg.attr("data-animate", element.images.fixed_height.url)
+                newimg.attr("data-state", "still");
+                newimg.addClass("giphy-image");
                 newdiv.append(newimg);
+                newdiv.addClass("giphy-div");
                 $("#imgrow").append(newdiv);
                 idfix = 0;
             }
         });
         if (wasdupe > 1) {
-            idfix += 10;
+            idfix += wasdupe;
             var queryurl = 'https://api.giphy.com/v1/gifs/search?q=' + tagstr + '&limit=' + (limit + idfix) + '&api_key=S6mnLoC0b3UsNPPt5Xg3bDY2XAXQux8P';
             console.log(queryurl);
             $.ajax({
@@ -56,10 +61,14 @@ function display(tagstr) {
                         ids.push(element2.id);
                         var newdiv = $("<div>");
                         var newstr = $("<p>");
-                        newstr.text(element2.rating);
+                        newstr.text("Rating: "+element2.rating);
                         newdiv.append(newstr);
-                        var newimg = $("<img>").attr("src", element2.images.fixed_height.url);
+                        var newimg = $("<img>").attr("src", element2.images.fixed_height_still.url);
+                        newimg.attr("data-still", element2.images.fixed_height_still.url);
+                        newimg.attr("data-animate", element2.images.fixed_height.url)
+                        newimg.attr("data-state", "still");
                         newdiv.append(newimg);
+                        newdiv.addClass("giphy-div");
                         $("#imgrow").append(newdiv);
                     }
                 });
@@ -85,5 +94,17 @@ $(window).on("load", function () {
         var newTag = $("#tag-input").val().trim();
         $("#tag-input").val('');
         addTag(newTag);
+    });
+
+    $(document).on("click", ".giphy-image", function(){
+        console.log($(this).attr("data-state"));
+        if($(this).attr("data-state")==="still"){
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else{
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     });
 });
